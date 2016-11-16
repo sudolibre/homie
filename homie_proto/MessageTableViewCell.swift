dd2c7288779202c2931eaebbe83c2297e76e6e75
sudolibre <optout.breaker@gmail.com>
2016-11-16 11:42:26 -0500

    files were not staged properly.

--- homie_proto/MessageTableViewCell.swift
+++ homie_proto/MessageTableViewCell.swift
@@ -1,24 +1,52 @@
-//
-//  MessageTableViewCell.swift
-//  homie_proto
-//
-//  Created by Jonathon Day on 11/7/16.
-//  Copyright © 2016 dayj. All rights reserved.
-//
-
 import UIKit
+import SnapKit
 
 class MessageTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    
+    lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.subheadline)
+        label.textColor = UIColor.blue
+        return label
+    }()
+    
+    lazy var bodyLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
+        label.textColor = UIColor.white
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        //self.isOwner = isOwner
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureSubviews()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    func configureSubviews() {
+        self.addSubview(self.nameLabel)
+        self.addSubview(self.bodyLabel)
 
-        // Configure the view for the selected state
+        nameLabel.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(self).offset(10)
+            make.left.equalTo(self).offset(20)
+            make.right.equalTo(self).offset(-20)
+            }
+
+        bodyLabel.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(nameLabel.snp.bottom).offset(1)
+            make.left.equalTo(self).offset(20)
+            make.right.equalTo(self).offset(-20)
+            make.bottom.equalTo(self).offset(-10)
+            
+        }
+        
     }
-
+    // We won’t use this but it’s required for the class to compile
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 }
