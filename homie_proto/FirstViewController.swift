850f6a116c71e84b61695094f6748ba0a1740540
sudolibre <optout.breaker@gmail.com>
2016-11-16 10:22:00 -0500

    first commit

---
+++ homie_proto/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  homie_proto
+//
+//  Created by Jonathon Day on 11/4/16.
+//  Copyright © 2016 dayj. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ homie_proto/MessageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageTableViewCell.swift
+//  homie_proto
+//
+//  Created by Jonathon Day on 11/7/16.
+//  Copyright © 2016 dayj. All rights reserved.
+//
+
+import UIKit
+
+class MessageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
